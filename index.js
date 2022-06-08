bfc72541d9da422aaafb6291b87b67b556cbb35d
Joey Quandt <joeyquandt@outlook.com>
2022-06-08 10:32:57 +0200

    veranderingen

--- index.js
+++
@@ -1,9 +0,0 @@
-$ = (queryString) => document.querySelector(queryString);
-
-const animate = () =>{
-    $('a-sphere').setAttribute('color','blue');
-    requestAnimationFrame(animate);
-}
-
-
-requestAnimationFrame(animate);
